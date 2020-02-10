358f32189141a06d9a318117c2822d64c8f646a4
Elliiee <danyiluozhang@gmail.com>
2020-02-10 16:33:06 -0600

    Traverse

---
+++ Traverse/src/LevelOrderIteration.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderIteration {
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> levels = new ArrayList<>();
+
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while (!queue.isEmpty()){
+            levels.add(new ArrayList<>());
+
+            int level_length = queue.size();
+            for (int i = 0; i < level_length; i++){
+                TreeNode node = queue.remove();
+                levels.get(level).add(node.val);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            level++;
+        }
+        return levels;
+    }
+}
