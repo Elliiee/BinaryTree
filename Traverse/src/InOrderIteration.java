af60a79a2fb401a9e23d74c5396d640fdc842225
Elliiee <danyiluozhang@gmail.com>
2020-02-10 15:34:17 -0600

    Traverse Iteration

--- Traverse/src/InOrderIteration.java
+++ Traverse/src/InOrderIteration.java
@@ -21,4 +21,21 @@ public class InOrderIteration {
         return output;
     }
 
+    /*
+    Input: [1, null, 2, 3]
+     */
+    public static void main(String[] args){
+        InOrderIteration ii = new InOrderIteration();
+
+        TreeNode root = new TreeNode(1);
+        root.left = null;
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        List<Integer> result = ii.inorderTraversal(root);
+
+        for (Integer r : result)
+            System.out.print(r + " ");
+    }
+
 }
