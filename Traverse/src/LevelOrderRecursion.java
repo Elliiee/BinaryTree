df3c7973bf4c143e70abd94d8dd44ab92e253a51
Elliiee <danyiluozhang@gmail.com>
2020-02-10 16:33:23 -0600

    Traverse

---
+++ Traverse/src/LevelOrderRecursion.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LevelOrderRecursion {
+    List<List<Integer>> levels = new ArrayList<>();
+
+    public List<List<Integer>> levelOrder(TreeNode root){
+        if (root == null) return levels;
+        helper(root, 0);
+        return levels;
+    }
+
+    public void helper(TreeNode node, int level){
+        if (levels.size() == level)
+            levels.add(new ArrayList<>());
+
+        levels.get(level).add(node.val);
+
+        if (node.left != null) helper(node.left, level + 1);
+        if (node.right != null) helper(node.right, level +1);
+    }
+}
